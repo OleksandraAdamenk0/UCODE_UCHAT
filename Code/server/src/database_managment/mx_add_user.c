660d79277cdb6d17730e1b624d14e6a586178470
mchaban <Maksym.Chaban@cs.khpi.edu.ua>
2024-11-18 19:11:51 +0200

    some corrections in add_user

--- Code/server/src/database_managment/mx_add_user.c
+++ Code/server/src/database_managment/mx_add_user.c
@@ -11,4 +11,11 @@ void mx_add_user(sqlite3 *db, cJSON *request) {
 
     snprintf(sql, sizeof(sql), "INSERT INTO Users (username, password, email, phone) VALUES ('%s', '%s', '%s', '%s')",
         username->string, password->string, email->string, phone->string);
+
+    if (sqlite3_exec(db, sql, 0, 0, &errMsg) != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", errMsg);
+        sqlite3_free(errMsg);
+    } else {
+        printf("User hasn't been added\n", username->string);
+    }
 }
