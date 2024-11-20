73718c75e7dfa3b6e5e70dfa0bb8c2848fa921af
mchaban <Maksym.Chaban@cs.khpi.edu.ua>
2024-11-20 12:58:58 +0200

    corrected response status in different situations and fixed issues with photo saving

--- Code/server/inc/database_managment.h
+++ Code/server/inc/database_managment.h
@@ -1,9 +1,12 @@
 #ifndef DATABASE_MANAGMENT_H
 #define DATABASE_MANAGMENT_H
 
-#include <sqlite3.h>
-#include <cjson/cJSON.h>
+#include "sqlite3.h"
+#include "cJSON.h"
 #include <stdio.h>
+#include "libmx.h"
+#include "cdecode.h"
+#include "cencode.h"
 
 void mx_create_server_tables(sqlite3 *db);
 int mx_register_user(sqlite3 *db, const cJSON *request);
--- Code/server/src/database_managment/mx_register_user.c
+++ Code/server/src/database_managment/mx_register_user.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 #include "database_managment.h"
 
 int mx_register_user(sqlite3 *db, const cJSON *request) {
@@ -16,24 +14,40 @@ int mx_register_user(sqlite3 *db, const cJSON *request) {
     cJSON *password = cJSON_GetObjectItemCaseSensitive(request, "password");
     cJSON *phone = cJSON_GetObjectItemCaseSensitive(request, "phone");
     cJSON *email = cJSON_GetObjectItemCaseSensitive(request, "email");
-    // cJSON *photo = cJSON_GetObjectItemCaseSensitive(request, "photo");
-
-    printf("%s\n", username->valuestring);
-    printf("%s\n", password->valuestring);
-    printf("%s\n", phone->valuestring);
-    printf("%s\n", email->valuestring);
-    // printf("%s\n", photo->valuestring);
+    cJSON *photo = cJSON_GetObjectItemCaseSensitive(request, "photo");
 
     sqlite3_bind_text(stmt, 1, username->valuestring, -1, SQLITE_STATIC);
     sqlite3_bind_text(stmt, 2, password->valuestring, -1, SQLITE_STATIC);
-    sqlite3_bind_text(stmt, 3, phone->valuestring, -1, SQLITE_STATIC);
-    sqlite3_bind_text(stmt, 4, email->valuestring, -1, SQLITE_STATIC);
-    // sqlite3_bind_text(stmt, 5, photo->valuestring, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, email->valuestring, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 4, phone->valuestring, -1, SQLITE_STATIC);
+
+    if (photo != NULL) {
+        size_t photo_size = sizeof(photo->valuestring);
+        unsigned char *photo_data = base64_decode(photo->valuestring, photo_size, &photo_size); // Implement this function
+
+        if (photo_data != NULL) {
+            sqlite3_bind_blob(stmt, 5, photo_data, photo_size, SQLITE_STATIC);
+            free(photo_data);
+        } else {
+            fprintf(stderr, "Failed to decode photo data.\n");
+            sqlite3_finalize(stmt);
+            return -1;
+        }
+    } else {
+        // Bind NULL if no photo is provided
+        sqlite3_bind_null(stmt, 5);
+    }
 
     if (sqlite3_step(stmt) != SQLITE_DONE) {
         fprintf(stderr, "Execution failed: %s\n", sqlite3_errmsg(db));
         sqlite3_finalize(stmt);
-        return -2;
+        if (mx_strcmp("UNIQUE constraint failed: users.username", sqlite3_errmsg(db)) == 0) {
+            return -2;
+        } else if (mx_strcmp("UNIQUE constraint failed: users.email", sqlite3_errmsg(db)) == 0) {
+            return -7;
+        } else if (mx_strcmp("UNIQUE constraint failed: users.phone", sqlite3_errmsg(db)) == 0) {
+            return -8;
+        }
     } else {
         printf("User  inserted successfully.\n");
     }
