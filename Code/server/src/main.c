c6493c2ace6f54ca7d9dae44ac9d9c7cdce36b9e
OleksandraAdamenk0 <oleksandra.adamenko@cs.khpi.edu.ua>
2024-11-22 07:00:04 +0000

    f

--- Code/server/src/main.c
+++ Code/server/src/main.c
@@ -1,12 +1,13 @@
-#include "../inc/server.h"
+#include "server.h"
 
 int main(int argc, const char * argv[]) {
     if (mx_init_server(argc, argv) == -1) {
-      mx_printerr("Usage: ./uchat_server <port>\n");
-    }
+        mx_printerr("Usage: ./uchat_server <port>\n");
+        return -1;
+    } else logger_info("server initialized\n");
 
     if (mx_open_connection() < 0) return -1;
-    else logger_info("connection opened\n");
+    else logger_info("connection channel opened\n");
 
     mx_accept_connections();
 }
