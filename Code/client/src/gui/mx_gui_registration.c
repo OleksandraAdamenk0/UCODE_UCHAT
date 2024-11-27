419c6b48385b0d99406e013e331e5dcf2921f8cb
vpedenko <vpedenko@stud.khpi.ucode-connect.study>
2024-11-27 01:52:51 +0100

    vscode fix

---
+++ Code/client/src/gui/mx_gui_registration.c
@@ -0,0 +1,22 @@
+#include "client.h"
+#include "gui.h"
+
+int mx_gui_network_error() {
+    // if (window == NULL) mx_create_window();
+    if (!window) {
+        logger_fatal("application window was not created"
+                     " before trying to show screen\n");
+        return -1;
+    }
+
+    char *path1 = mx_get_cur_dir();
+    char *path = mx_strjoin(path1, REGISTRATION_HTML);
+    free(path1);
+
+    WebKitWebView *webview = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    webkit_web_view_load_uri(webview, path);
+
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(webview));
+    gtk_widget_show_all(window);
+    return 0;
+}
